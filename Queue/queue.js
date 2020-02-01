c8ba1824aaa378551fa053d5cfbe81a819dfe445
niv b <nivb52@gmail.com>
2020-02-01 18:21:18 +0200

    queue - data stracture

---
+++ Queue/queue.js
@@ -0,0 +1,35 @@
+// Queues
+// obey the rule of FIFO - first in first out
+
+module.exports =  {createQueue}
+
+function createQueue() {
+  const queue = [];
+
+  return {
+    enqueue(item) {
+      queue.unshift(item);
+    },
+
+    dequeue() {
+      queue.pop();
+    },
+
+    peek() {
+      return queue[queue.length - 1];
+    },
+
+    // get give us the corrent state, and not the state at the init (0)
+    get length() {
+      return queue.length;
+    },
+
+    isEmpty() {
+      return queue.length === 0;
+    },
+
+    len() {
+      return queue.length;
+    }
+  };
+}
