b738426143dd7e710529df9e2513d598c9d6f2ef
niv b <nivb52@gmail.com>
2020-02-01 18:21:52 +0200

    first commit

---
+++ Queue/priorityQueue.js
@@ -0,0 +1,35 @@
+const { createQueue } = require("./queue");
+
+module.exports = { priorityQueue };
+
+function priorityQueue(params) {
+  const highPriority = createQueue();
+  const lowPriority = createQueue();
+  return {
+    enqueue(item, priority = "low") {
+      if (item === null) return;
+
+      if (priority === "high") {
+        highPriority.enqueue(item);
+      } else lowPriority.enqueue(item);
+    },
+    dequeue(priority = "high") {
+      if (priority === "high") {
+        highPriority.dequeue();
+      } else lowPriority.dequeue();
+    },
+    peek() {
+      return highPriority.length === 0
+        ? lowPriority.peek()
+        : highPriority.peek();
+    },
+    get length() {
+      return highPriority.length === 0
+        ? highPriority.length
+        : lowPriority.length;
+    },
+    isEmpty() {
+      return highPriority.length === 0 && lowPriority.length === 0;
+    }
+  };
+}
